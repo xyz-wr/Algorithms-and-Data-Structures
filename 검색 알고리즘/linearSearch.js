907a666f9a91f5bfe5b8f16e4c5e8b86c262b5c5
xyz-wr <pponi13468@naver.com>
2022-07-27 12:32:38 +0900

    검색

---
+++ 검색 알고리즘/linearSearch.js
@@ -0,0 +1,10 @@
+function linearSearch(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+linearSearch([10, 15, 20, 25, 30], 15);
