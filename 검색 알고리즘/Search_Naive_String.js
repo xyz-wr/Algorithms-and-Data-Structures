dcbed94d77af1de70e254c6c1e50be6ad8805678
xyz-wr <pponi13468@naver.com>
2022-07-28 13:23:01 +0900

    Search_Naive_String

---
+++ 검색 알고리즘/Search_Naive_String.js
@@ -0,0 +1,13 @@
+function naiveSearch(long, short) {
+  let cnt = 0;
+
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i+j]) break;
+      if (j === short.length - 1) cnt++; 
+    }
+  }
+  return cnt;
+}
+
+naiveSearch("lorie loled", "lo");
